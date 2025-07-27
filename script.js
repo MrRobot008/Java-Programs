873f789505f02f0706a4dd7bf7bd6b699ac7da9e
Siddharth <siddharth.shettar@gmail.com>
2025-07-27 20:37:41 +0530

    This is the accompanied script

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("It is working");
+document.getElementById("button").onclick = function() {
+    alert("Button not clicked!");
+};
+newbtn.innerHTML = "Click Me";
+button.style.backgroundColor = "#e69e23ff";
+newbtn.style.color = "white";
+newbtn.style.padding = "10px 30px";
+document.getElementById("submitButton").onclick = function() {
+    alert("Form submitted successfully!");
+}
+submitbutton.style.backgroundColor = "#4CAF50";
+submitbutton.style.color = "white"; 
+
+
